260875a2f26071a736fcb8a3db4d330f97ca9aa8
Imuratore-bio <isabella.b.muratore@gmail.com>
2021-12-16 13:37:37 -0500

    Initial commit

--- R/blob_counter.R
+++ R/blob_counter.R
@@ -2,8 +2,6 @@
 #'
 #' @param mask_file the mask file used with trackR
 #' @param tracks_file the trajectory csv output file from trackR
-#' @param corner_thres how sensitive the corner detection is
-#' @param centroid_similarity_thres how similar centroids can be to each other without being discarded
 #'
 #' @return a data frame
 #' @export
@@ -14,7 +12,7 @@
 #' blob_counter(mask_file_input, tracks_file_input)
 
 #beginning of function
-blob_counter <- function(mask_file, tracks_file, corner_thres = 120, centroid_similarity_thres = 10) {
+blob_counter <- function(mask_file, tracks_file) {
 
 
   #out of all blobs created, how many unique ones have a position close to the centroid of a given box?
@@ -23,36 +21,35 @@ blob_counter <- function(mask_file, tracks_file, corner_thres = 120, centroid_si
   requireNamespace("image.ContourDetector")
   requireNamespace("magick")
   requireNamespace("pixmap")
-  requireNamespace("image.CornerDetectionF9")
+  requireNamespace("grDevices")
+  requireNamespace("image.CornerDetectionHarris")
+  requireNamespace("graphics")
 
   #read in tracks file
-  tracks <- read.csv(file = tracks_file)
+  tracks <- utils::read.csv(file = tracks_file)
 
   #read in mask file
-  image_x <- image_read(mask_file)
+  image_x <- magick::image_read(mask_file)
 
-  #convert to pgm to use with corner detector
-  x <- image_convert(image_x, format = "pgm", depth = 8)
-  f <- tempfile(fileext = ".pgm")
-  image_write(x, path = f, format = "pgm")
-  m <- read.pnm(file = f, cellres = 1)
+  #convert to magick format to use with corner detector
+  x <- magick::image_convert(image_x, format = "pgm", depth = 8)
 
-  #define centroid points for the four boxes in mask file
+  #detect corners
+  corners <- image.CornerDetectionHarris::image_harris(x)
 
-  #get corners
-  #can adjust this threshold if we don't get the expected 16 corners for 4 squares
-  corners <- image_detect_corners(m@grey * 255,
-                                  threshold = corner_thres,
-                                  suppress_non_max = TRUE)
+  #for some reason the Harris corner detector results are vertically mirrored; this fixes that
+  corners$y <- 1080 - corners$y
 
-  #warnings to user
-  if (length(corners$x) > 16) {
-    print("Warning, corner threshold too low, adjust up")
+  #warning to user
+  if (length(corners$x) != 16) {
+    print("Warning, desired number of corners is 16. Current number of corners is:")
+    print(length(corners$x))
+
   }
 
-  if (length(corners$x) < 16) {
-    print("Warning, corner threshold too high, adjust down")
-  }
+  #view identified corners
+  plot(x)
+  graphics::points(corners$x, corners$y, col = "red", pch = 20, lwd = 0.5)
 
   #define a list to store corner coordinates
   line_coords <- c()
@@ -88,7 +85,56 @@ blob_counter <- function(mask_file, tracks_file, corner_thres = 120, centroid_si
   int <- as.numeric(interaction(mn, mx))
   no_dupes <- ordered[match(unique(int), int),]
 
-  short_lines <- no_dupes[1:16,]
+  no_dupes$mx <- (no_dupes$x1 + no_dupes$x2)/2
+  no_dupes$my <- (no_dupes$y1 + no_dupes$y2)/2
+
+  #remove any lines where the middle area is black pixels
+
+  #read in file again in a different format to extract pixel hex values
+  img <- png::readPNG(mask_file)
+  y <- grDevices::rgb(img[,,1], img[,,2], img[,,3], alpha = img[,,4])
+  yg <- colorspace::desaturate(y)
+  yn <- grDevices::col2rgb(yg)[1, ]/255
+  dim(y) <- dim(yg) <- dim(yn) <- dim(img)[1:2]
+
+  #needs to be transposed
+  y <- t(y)
+
+  #test whether all midpoints have at least one pure white pixel nearby
+  #this is to exclude any short lines connecting points outside of squares
+
+  #make empty variable to hold list of True/False values
+  delete <- c()
+
+  #for each set of line midpoints
+  for (line in 1:length(no_dupes$mx)){
+    #test whether a midpoint itself or at least one pixel in any cardinal direction going out ten pixels is pure white
+    delete <- c(delete,  (y[round(no_dupes$mx[line]), (1080-round(no_dupes$my[line]))] == "#FFFFFFFF" | y[(round(no_dupes$mx[line])+10), (1080-round(no_dupes$my[line])+10)] == "#FFFFFFFF" | y[(round(no_dupes$mx[line])+10), (1080-round(no_dupes$my[line])-10)] == "#FFFFFFFF"| y[(round(no_dupes$mx[line])-10), (1080-round(no_dupes$my[line])+10)] == "#FFFFFFFF"| y[(round(no_dupes$mx[line])-10), (1080-round(no_dupes$my[line])-10)] == "#FFFFFFFF"))
+
+  }
+
+  #if there was at least one white pixel, keep that line, but exclude others
+  no_dupes <- no_dupes[delete,]
+
+  #take only the 16 shortest lines to get outlines of squares
+
+  #reset row names since some rows were deleted before
+  row.names(no_dupes) <- NULL
+
+  #or fewer than 16 if there were fewer than 16 corners detected
+  if (length(corners$x) < 16){
+  dupe_limit <- 16 - ((16 - length(corners$x))*2)
+
+  } else {
+  dupe_limit <- 16
+  }
+
+  short_lines <- no_dupes[1:dupe_limit,]
+
+  #view line segments
+  plot(x)
+  graphics::segments(short_lines$x1, short_lines$y1, short_lines$x2, short_lines$y2, col = "red", lwd = 0.5)
+
 
   #for selected point combos that share one point, or have an extremely similar point
   for (line1 in (1:length(short_lines$x1))) {
@@ -130,7 +176,7 @@ blob_counter <- function(mask_file, tracks_file, corner_thres = 120, centroid_si
   for (c in (1:length(short_lines$cx))) {
     for (c2 in (1:length(short_lines$cx))) {
       #see if they're similar
-      if (abs(short_lines$cx[c2]-short_lines$cx[c]) < centroid_similarity_thres | abs(short_lines$cy[c2]-short_lines$cy[c]) < centroid_similarity_thres) {
+      if (abs(short_lines$cx[c2]-short_lines$cx[c]) < 5 | abs(short_lines$cy[c2]-short_lines$cy[c]) < 5) {
         #replace one with the other
         short_lines$cx[c2] <- short_lines$cx[c]
         short_lines$cy[c2] <- short_lines$cy[c]
@@ -138,15 +184,15 @@ blob_counter <- function(mask_file, tracks_file, corner_thres = 120, centroid_si
     }
   }
 
-  #user warnings about selecting centroid similarity threshold
-  #you should end up with 4 unique centroid points so if you don't you need to adjust
-  if (length( short_lines[!duplicated(short_lines[c("cx","cy")]),]$cx  ) > 4) {
-    print("Warning, centroid similarity threshold too low, adjust up")
+  #user warnings about centroid similarity threshold
+  if (length( short_lines[!duplicated(short_lines[c("cx","cy")]),]$cx  ) != 4) {
+    print("Warning, desired number of centroids is 4. Current number of centroids is:")
+    print(length( short_lines[!duplicated(short_lines[c("cx","cy")]),]$cx  ))
   }
 
-  if (length( short_lines[!duplicated(short_lines[c("cx","cy")]),]$cx  ) < 4) {
-    print("Warning, centroid similarity threshold too high, adjust down")
-  }
+  #view the final centroids
+  plot(x)
+  graphics::points(short_lines$cx, short_lines$cy, col = "red", pch = 20, lwd = 0.5)
 
   #variables to hold column data for output csv
   #centroid position and count of blobs that pass near it
@@ -163,14 +209,35 @@ blob_counter <- function(mask_file, tracks_file, corner_thres = 120, centroid_si
     #for each row in the tracks file
     for (p in 1:length(tracks$x)) {
       #if it is close to the centroid in question and not already counted
-      if ((abs(tracks$x[p] - short_lines[!duplicated(short_lines[c("cx","cy")]),]$cx[c]) < centroid_proximity_thres) & (abs(tracks$y[p] - short_lines[!duplicated(short_lines[c("cx","cy")]),]$cy[c]) < centroid_proximity_thres) & !(tracks$track[p] %in% used_blobs)) {
-        #add the name to the already counted list
-        used_blobs <- append(used_blobs, tracks$track[p])
-        #up the blob count
-        blob_count <- blob_count + 1
-      }
-    }
+      #and closer to this centroid than to any other centroid
+      if (!(tracks$track[p] %in% used_blobs) & (abs(tracks$x[p] - short_lines[!duplicated(short_lines[c("cx","cy")]),]$cx[c]) < centroid_proximity_thres) & (abs(tracks$y[p] - short_lines[!duplicated(short_lines[c("cx","cy")]),]$cy[c]) < centroid_proximity_thres)) {
 
+        #set this to False unless a point is found to be closer to a given centroid than to all others
+        comparison_check <- FALSE
+
+        #compare to all non-focal centroids
+        for (others in 1:length(short_lines[!duplicated(short_lines[c("cx","cy")]),]$cx)) {
+
+          #checking distance between each point and a given centroid
+          #is it closer to the focal centroid than to any other centroid?
+          if( sqrt((tracks$y[p] - short_lines[!duplicated(short_lines[c("cx","cy")]),]$cy[c])^2 + (tracks$x[p] - short_lines[!duplicated(short_lines[c("cx","cy")]),]$cx[c])^2) < sqrt((tracks$y[p] - short_lines[!duplicated(short_lines[c("cx","cy")]),]$cy[others])^2 + (tracks$x[p] - short_lines[!duplicated(short_lines[c("cx","cy")]),]$cx[others])^2 ) ) {
+
+            comparison_check <- TRUE
+
+          }
+
+        }
+
+        #add the name to the already counted list
+        if (comparison_check == TRUE) {
+          used_blobs <- append(used_blobs, tracks$track[p])
+          #up the blob count
+          blob_count <- blob_count + 1
+        }
+      }
+
+    }
+    #finishing the above loops takes the longest
     #based on result of above loop append row to variables holding data for data frame
     centroid_x_value <- append(centroid_x_value, unique(short_lines$cx)[c])
     centroid_y_value <- append(centroid_y_value, unique(short_lines$cy)[c])
@@ -179,7 +246,9 @@ blob_counter <- function(mask_file, tracks_file, corner_thres = 120, centroid_si
 
   #create and return output data frame
   output <- data.frame(centroid_x_value, centroid_y_value, count)
-  return(output)
+  #sort by x value so it's basically displaying the results for each square from left to right
+  output_sorted <- output[order(centroid_x_value),]
+  return(output_sorted)
 
   #end of function
 }
